8f1b39c5a99494a6a3a382dbcd84ac43a244f715
AnthonyDevops <112930857+AnthonyDevops@users.noreply.github.com>
2023-10-18 19:58:41 +0200

    DWA5_Challenge1

---
+++ DWA_5_Challenge1/scripts.js
@@ -0,0 +1,23 @@
+// scripts.js
+
+const form = document.querySelector("[data-form]");
+const result = document.querySelector("[data-result]");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const entries = new FormData(event.target);
+  const { dividend, divider } = Object.fromEntries(entries);
+
+if (isNaN(dividend) || isNaN(divider) || divider == "") {
+    result.innerText = "Division not performed. Both values are required in inputs. Try Again";
+}
+if(isNaN(divider) || parseFloat(divider) < 0){ 
+result.innerText = "Division not performed. Invalid number provided. Try again";
+  }
+  if (isNaN(dividend) || isNaN(divider)) {
+    result.innerText = ("Something critical went wrong. Please reload the page.");
+  } else {
+result.innerText = parseInt(dividend / divider);
+  }
+});
+
